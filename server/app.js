04cc984de7d8de0932657ad6f1acd8269f9c8f88
joshua-frey-wsu <joshua.h.frey@gmail.com>
2026-04-27 16:13:25 -0700

    Setup Express app for API

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+require('dotenv').config({
+    path: process.env.NODE_ENV ? '../.env' : '../.env.dev',
+})
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/status', (req, res) => {
+    res.json({
+        status: 'Running',
+        timestamp: new Date().toISOString()
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
