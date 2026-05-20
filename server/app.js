806a39e3c4a9d1602993bc822e079a04a83a937b
joshua-frey-wsu <joshua.h.frey@gmail.com>
2026-05-19 17:20:57 -0700

    Implemented error converter middlewar function to convert library errors to proper apierror format

--- server/app.js
+++ server/app.js
@@ -1,5 +1,9 @@
 import express from 'express';
 import memberRoutes from './routes/member.routes.js';
+import { asyncHandler } from './middlewares/asyncHandler.js';
+import { errorConverter } from './middlewares/errorConverter.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { NotFoundError } from './helpers/ApiError.js';
 
 const app = express();
 
@@ -16,6 +20,15 @@ app.get('/status', (req, res) => {
         timestamp: new Date().toISOString()
     });
 });
+
+// 404 Handler (Catch-all)
+app.use((req, res, next) => {
+    next(new NotFoundError(`Route ${req.originalUrl}`));
+});
+
+// Error handling middleware pipeline
+app.use(errorConverter); // Convert library/db erros to ApiError first
+app.use(errorHandler); // Format and send error response
 
 const PORT = process.env.SERVER_PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ server/middlewares/errorConverter.js
@@ -0,0 +1,32 @@
+/**
+ * Handles specific error types like errors from libraries or databases.
+ * These type of errors nee to be converted to fit the custom error format created.
+ */
+
+import { ApiError, ValidationError, ConflictError } from "../helpers/ApiError";
+
+/**
+ * Converts known error types to ApiError instances.
+ * This middleware sits before the main error handler
+ */
+export const errorConverter = (err, req, res, next) => {
+    let convertedError = err;
+
+    // TODO - Handle Postgres errors
+
+    // Handle JSON parse errors
+    if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+        convertedError = new ApiError('Invalid JSON in request body', 400, 'INVALID_JSON');
+    }
+
+    // Handle JWT errors
+    if (err.name === 'JsonWebTokenError') {
+        convertedError = new ApiError('Invalid token', 401, 'INVALID_TOKEN');
+    }
+
+    if (err.name === 'TokenExpiredError') {
+        convertedError = new ApiError('Token expired', 401, 'TOKEN_EXPIRED');
+    }
+
+    next(convertedError); // passes it down to next middleware (errorHandler)
+}
